18bcdc7c4d0f617684f21f447313838e34f29364
gauravd03 <gauravdandekar0071@gmail.com>
2024-01-04 14:46:21 +0530

    Framework structure upload

---
+++ com.HybridFrameworkDevelopment1/src/main/java/com/HybridFrameworkDevelopment1/com/HybridFrameworkDevelopment1/App.java
@@ -0,0 +1,13 @@
+package com.HybridFrameworkDevelopment1.com.HybridFrameworkDevelopment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
